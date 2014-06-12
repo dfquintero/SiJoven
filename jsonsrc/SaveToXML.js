50f1fabf382b8adb231e8c63bf5a6fdd669fc95c
dfquintero <diferquintero@hotmail.com>
2014-06-12 03:10:52 -0500

    commit -Marylin

---
+++ jsonsrc/SaveToXML.js
@@ -0,0 +1,76 @@
+﻿var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var FILENAME = 'Data.xml';
+
+
+$('form').submit(function( ev ){
+    ev.preventDefault(); //I keep form from submitting
+    $( xmlDocument ).find('Person').attr({
+        username: $("input[name=usrname]"),
+            password: $("input[name=pswd]"),
+            //and so on
+            });
+
+});
+
+function SaveXML(UserData) {
+    var file = fso.CreateTextFile(FILENAME, true);
+    file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
+    file.WriteLine('<PersonInfo>\n');
+
+    for (countr = 0; countr < UserData.length; countr++) {
+        file.Write('    <Info ');
+        file.Write('Aprende="' + UserData[countr][0] + '" ');
+        file.Write('Descubre="' + UserData[countr][1] + '" ');
+        file.Write('Cuidate="' + UserData[countr][2] + '" ');
+        file.Write('Diviertete="' + UserData[countr][3] + '" ');
+        file.Write('Involucrate="' + UserData[countr][4] + '" ');
+        file.Write('Animate="' + UserData[countr][5] + '" ');
+        file.Write('Edad="' + UserData[countr][6] + '" ');
+        file.Write('Ubicacion="' + UserData[countr][7] + '"');
+        file.Write('Pais="' + UserData[countr][8] + '"');
+        file.Write('Departamento="' + UserData[countr][9] + '"');
+        file.Write('Ciudad="' + UserData[countr][10] + '"');
+        file.WriteLine('></Info>\n');
+    } // end for countr
+
+    file.WriteLine('</PersonInfo>\n');
+    file.Close();
+
+} // end SaveXML function --------------------
+
+//function LoadXML(xmlFile) {
+//    xmlDoc.load(xmlFile);
+//    return xmlDoc.documentElement;
+//} //end function LoadXML()
+
+//function initialize_array() {
+//    var person = new Array();
+//    var noFile = true;
+//    var xmlObj;
+//    if (fso.FileExists(FILENAME)) {
+//        xmlObj = LoadXML(FILENAME);
+//        noFile = false;
+//    } // if
+//    else {
+//        xmlObj = LoadXML("Data.xml");
+//        //alert("local" + xmlObj);
+//    } // end if
+
+//    var usrCount = 0;
+//    while (usrCount < xmlObj.childNodes.length) {
+//        var tmpUsrs = new Array(xmlObj.childNodes(usrCount).getAttribute("Usrname"),
+//                                xmlObj.childNodes(usrCount).getAttribute("Pswd"),
+//                    xmlObj.childNodes(usrCount).getAttribute("PersonID"),
+//                                xmlObj.childNodes(usrCount).getAttribute("FirstName"),
+//                                xmlObj.childNodes(usrCount).getAttribute("LastName"),
+//                                xmlObj.childNodes(usrCount).getAttribute("Gender"),
+//                                xmlObj.childNodes(usrCount).getAttribute("DOB"),
+//                                xmlObj.childNodes(usrCount).getAttribute("Title"));
+//        person.push(tmpUsrs);
+//        usrCount++;
+//    }   //end while
+//    if (noFile == false)
+//        fso.DeleteFile(FILENAME);
+//    SaveXML(person);
+//}   
---
+++ jsonsrc/jsoncallold.js
@@ -0,0 +1,286 @@
+﻿function _popup(id) {
+
+    if (confirm(this.id)) {
+        localStorage.setItem("id", id);
+    }
+}
+
+var allData;
+
+function getDataFromSvc(){
+    console.log("all data from svc");
+    $.ajax({
+
+        dataType: "json",
+        //url: 'ofertasServicio.json',
+        url: 'http://poi.colombiajoven.gov.co/api/oferta',
+    }).done(function (data) {
+        allData = data;
+        console.log("data : done");
+       
+    });
+   
+    
+}
+
+function sortByKey(array, key) {
+    return array.sort(function (a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+function seccionIMG(seccion) {
+
+    switch (seccion) {
+        case 1: img = '1ico_edu.png';
+            break;
+        case 2: img = '2ico_emp.png';
+            break;
+        case 3: img = '3ico_pre.png';
+            break;
+        case 4: img = '4ico_rec.png';
+            break;
+        case 5: img = '5ico_vol.png';
+            break;
+        case 6: img = '6ico_des.png';
+            break;
+    }
+    return img;
+}
+
+function itemsPush(elemento) {
+   
+    console.log("items push method a elemento:" + elemento);
+    var items = [];
+    $.each(allData, function (key, val) {
+
+        var img = seccionIMG(val.FkSeccion)
+
+        if (val.Oportunidad.length > 0) {
+            
+            items.push("<li ><a href='#" + val.PkOferta + "_' value =" + val.PkOferta + " data-rel='popup' ><img src='img/ico/" + img + "' style='margin:10px; height: 60px; width: 60px;' /> <h1>" + val.Oportunidad + "</h1> <p>" + val.EntidadNombre + " - " + val.PoblacionObjetivo + " </p></a></li>");
+
+            items.push(" <div data-role='popup' style ='position: fixed; position: fixed; top: 10px;left: 10%;right: 10%;width: 80%; max-height: 90%' id='" + val.PkOferta + "_'>");
+            items.push("<center>");
+            items.push("<div data-role='header' data-theme='b'>");
+            items.push(" <div data-role='navbar'>");
+            items.push(" <ul class='ui-nodisc-icon'>");
+            items.push(" <li><a href='#' data-icon='user'></a></li>");
+            items.push(" <li><a href='#' data-icon='clock'></a></li>");
+            items.push(" <li><a href='#' data-icon='info'></a></li>");
+            items.push(" <li style ='background-color: darkred;'><a href='#popupcloseright' data-icon='delete' style ='background-color: darkred;'></a></li>");
+            items.push(" </ul>");
+            items.push(" </div>");
+            items.push(" </div>");
+            items.push(" <div data-role='content' id='popUpContent'>");
+            items.push(" <center>");
+            items.push(" <img src='img/ico/" + img + "' style=' max-height:80px' />");
+            items.push("</center>");
+            items.push("<h2 style='text-align:center; '>" + val.Oportunidad + "</h2>");
+            items.push("<p  style='text-align:justify; '>" + val.Informacion + "</p>");
+            items.push("</div>");
+            items.push("<div data-role='footer'>");
+            //items.push("<div class='ui-grid-a'>");
+            //items.push("<div class='ui-block-a' style='text-align: center; background-color:#55ACEE '>");
+            //var twurl = "https://twitter.com/intent/tweet?text=";
+            //items.push("<a href='" + twurl + val.Oportunidad + " " + val.UrlFuente + "' target='_blank'><img src='img/RS/twitter.png' style='max-height: 20px; padding: 5px; margin: 5px 5px;' /></a>");
+            //items.push("</div>");
+            //items.push("<div class='ui-block-b' style='text-align: center; background-color: #3b5998'>");
+            //var fburl = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=";
+            //items.push("<a href='" + fburl + val.UrlFuente + "' target='_blank'><img src='img/RS/facebook.png' style='max-height: 20px; padding: 5px;  margin: 5px 5px;' /></a>");
+            //items.push("</div>");
+            //items.push("</div>");
+            //items.push("<div class='ui-grid-b' style='text-align: center; background-color: #008511'>");
+            //items.push("<h3 style='text-align:center; color:white'>");
+            //items.push("<a style='text-decoration:none; color: white;' href='" + val.UrlFuente + "' target='_blank'>¡PARTICIPAR!</a>");
+            //items.push("</h3>");
+            //items.push("</div>");
+            items.push("</div>");
+            items.push("</div>")
+        }
+
+
+
+
+    });
+    var container = document.getElementById(elemento)
+    console.log(container);
+    $("<ul/>", {
+        "id": "eventList_" + elemento, "data-role": "listview", "data-inset": "true", "data-filter": "true",
+        html: items.join("")
+    }).appendTo(container);
+}
+
+function itemsPushWithFilter(categoria, elemento) {
+  
+    console.log("items push method");
+    var items = [];
+    //var info = sortByKey(data, 'OPORTUNIDAD');
+
+    $.each(allData, function (key, val) {
+
+
+        if (val.Seccion == categoria) {
+            var img = seccionIMG(val.FkSeccion)
+
+            if (val.Oportunidad.length > 0) {
+               
+                items.push("<li ><a href='#" + elemento + "_" + val.PkOferta + "_' value =" + val.PkOferta + " data-rel='popup' ><img src='img/ico/" + img + "' style='margin:10px; height: 60px; width: 60px;' /> <h1>" + val.Oportunidad + "</h1> <p>" + val.EntidadNombre + " - " + val.PoblacionObjetivo + " </p></a></li>");
+
+                items.push(" <div data-role='popup' style ='position: fixed; position: fixed; top: 10px;left: 10%;right: 10%;width: 80%;' id='" + elemento + "_" + val.PkOferta + "_'>");
+                items.push("<center>");
+                items.push("<div data-role='header'>");
+                items.push(" <div data-role='navbar'>");
+                items.push(" <ul>");
+                items.push(" <li><a href='#' data-icon='user'></a></li>");
+                items.push(" <li><a href='#' data-icon='clock'></a></li>");
+                items.push(" <li><a href='#' data-icon='info'></a></li>");
+                items.push(" <li><a href='#popupcloseright' data-icon='delete'></a></li>");
+                items.push(" </ul>");
+                items.push(" </div>");
+                items.push(" </div>");
+                items.push(" <div data-role='content' id='popUpContent'>");
+                items.push(" <center>");
+                items.push(" <img src='img/ico/" + img + "' style=' max-height:80px' />");
+                items.push("</center>");
+                items.push("<h2 style='text-align:center; text-overflow: ellipsis;width: 200px;'>" + val.Oportunidad + "</h2>");
+                items.push("<p  style='text-align:justify; text-overflow: ellipsis; width: 200px; '>" + val.Informacion + "</p>");
+                items.push("</div>");
+                items.push("<div data-role='footer'>");
+                items.push("<div class='ui-grid-a'>");
+                items.push("<div class='ui-block-a' style='text-align: center; background-color:#55ACEE '>");
+                var twurl = "https://twitter.com/intent/tweet?text=";
+                items.push("<a href='" + twurl + val.Oportunidad + " " + val.UrlFuente + "' target='_blank'><img src='img/RS/twitter.png' style='max-height: 20px; padding: 5px; margin: 5px 5px;' /></a>");
+                items.push("</div>");
+                items.push("<div class='ui-block-b' style='text-align: center; background-color: #3b5998'>");
+
+                var fburl = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=";
+                items.push("<a href='" + fburl + val.UrlFuente + "' target='_blank'><img src='img/RS/facebook.png' style='max-height: 20px; padding: 5px;  margin: 5px 5px;' /></a>");
+                items.push("</div>");
+                items.push("</div>");
+                items.push("<div class='ui-grid-b' style='text-align: center; background-color: #008511'>");
+                items.push("<h3 style='text-align:center; color:white'>");
+                items.push("<a style='text-decoration:none; color: white;' href='" + val.UrlFuente + "' target='_blank'>¡PARTICIPAR!</a>");
+                items.push("</h3>");
+                items.push("</div>");
+                items.push("</div>");
+                items.push("</center>");
+                items.push("</div>")
+            }
+        }
+
+
+
+    });
+    var container = document.getElementById(elemento)
+    $("<ul/>", {
+        "id": "eventList_" + elemento, "data-role": "listview", "data-inset": "true", "data-filter": "true",
+        html: items.join("")
+    }).appendTo(container);
+}
+
+function filterResults(categoria, elemento) {
+    
+    console.log('filtro :' + categoria);
+    itemsPushWithFilter(categoria, elemento);
+    console.log('finish push filtro: ' + categoria);
+    
+   
+}
+
+function allResults(elemento) {
+   
+    console.log('todos los resultados');
+    itemsPush(elemento);
+
+    console.log('finish push todo');
+   
+}
+
+
+//$.ajax({
+
+//    dataType: "json",
+//    url:'ofertasServicio.json',
+//   // url: 'http://poi.colombiajoven.gov.co/api/oferta',
+//}).done(function (data) {
+//    var items = [];
+   
+//    //var info = sortByKey(data, 'OPORTUNIDAD');
+//    //console.log("sort")
+//    $.each(data, function (key, val) {
+
+//        var img = 'ico_300.png';
+//        console.log("each");
+//        console.log(val.Seccion);
+//        switch (val.Seccion) {
+
+//            case 'Aprende y Crece': img = '1ico_edu.png';
+//                break;
+//            case 'Descubre y trabaja': img = '2ico_emp.png';
+//                break;
+//            case 'Cuídate y Denuncia': img = '3ico_pre.png';
+//                break;
+//            case 'Diviértete y Disfruta': img = '4ico_rec.png';
+//                break;
+//            case 'Involúcrate y Ayuda': img = '5ico_vol.png';
+//                break;
+//            case 'Anímate y Participa': img = '6ico_des.png';
+//                break;
+
+
+//        }
+//        console.log(val.Oportunidad);
+//        if (val.Oportunidad.length > 0) {
+//            console.log("push");
+//            items.push("<li ><a href='#" + val.PkOferta + "_' value =" + val.PkOferta + " data-rel='popup' ><img src='img/ico/" + img + "' style='margin:10px; height: 60px; width: 60px;' /> <h1>" + val.Oportunidad + "</h1> <p>" + val.EntidadNombre + " - " + val.PoblacionObjetivo + " </p></a></li>");
+
+//            items.push(" <div data-role='popup' id='" + val.PkOferta + "_'>");
+//            items.push("<center>");
+//            items.push("<div data-role='header'>");
+//            items.push(" <div data-role='navbar'>");
+//            items.push(" <ul>");
+//            items.push(" <li><a href='#' data-icon='user'></a></li>");
+//            items.push(" <li><a href='#' data-icon='clock'></a></li>");
+//            items.push(" <li><a href='#' data-icon='info'></a></li>");
+//            items.push(" <li><a href='#popupcloseright' data-icon='delete'></a></li>");
+//            items.push(" </ul>");
+//            items.push(" </div>");
+//            items.push(" </div>");
+//            items.push(" <div data-role='content' id='popUpContent'>");
+//            items.push(" <center>");
+//            items.push(" <img src='img/ico/" + img + "' style=' max-height:80px' />");
+//            items.push("</center>");
+//            items.push("<h2 style='text-align:center; text-overflow: ellipsis;width: 200px;'>" + val.Oportunidad + "</h2>");
+//            items.push("<p  style='text-align:justify; text-overflow: ellipsis; width: 200px; '>" + val.Informacion + "</p>");
+//            items.push("</div>");
+//            items.push("<div data-role='footer'>");
+//            items.push("<div class='ui-grid-a'>");
+//            items.push("<div class='ui-block-a' style='text-align: center; background-color:#55ACEE '>");
+//            var twurl = "https://twitter.com/intent/tweet?text=";
+//            items.push("<a href='" + twurl + val.Oportunidad + " " + val.UrlFuente + "' target='_blank'><img src='img/RS/twitter.png' style='max-height: 20px; padding: 5px; margin: 5px 5px;' /></a>");
+//            items.push("</div>");
+//            items.push("<div class='ui-block-b' style='text-align: center; background-color: #3b5998'>");
+
+//            var fburl = "https://www.facebook.com/sharer/sharer.php?s=100&p[url]=";
+//            items.push("<a href='" + fburl + val.UrlFuente + "' target='_blank'><img src='img/RS/facebook.png' style='max-height: 20px; padding: 5px;  margin: 5px 5px;' /></a>");
+//            items.push("</div>");
+//            items.push("</div>");
+//            items.push("<div class='ui-grid-b' style='text-align: center; background-color: #008511'>");
+//            items.push("<h3 style='text-align:center; color:white'>");
+//            items.push("<a style='text-decoration:none; color: white;' href='" + val.UrlFuente + "' target='_blank'>¡PARTICIPAR!</a>");
+//            items.push("</h3>");
+//            items.push("</div>");
+//            items.push("</div>");
+//            items.push("</center>");
+//            items.push("</div>")
+//        }
+//    });
+//    var container = document.getElementById("allContent")
+//    $("<ul/>", {
+//        "id": "eventList", "data-role": "listview", "data-inset": "true", "data-filter": "true",
+//        html: items.join("")
+//    }).appendTo(container);
+//});
+
